05292b0780fd1ba67999810130f9e7c1ce952266
Peter Treitler <peter.treitler@axtesys.at>
2019-08-22 18:15:16 +0200

    Add WordRepository

---
+++ app/src/main/java/android/example/com/roomwordsample/WordRepository.kt
@@ -0,0 +1,13 @@
+package android.example.com.roomwordsample
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao: WordDao) {
+    val allWords: LiveData<List<Word>> = wordDao.getAllWords()
+
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
